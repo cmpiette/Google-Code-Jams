0377be2f6ac930f3368cbb4d2e4356646376ac45
Chris Piette <christopher.m.piette@gmail.com>
2018-06-17 14:51:24 -0400

    first two questions in qualifying round

---
+++ 2017/OversizedPancakeFlipper.cs
@@ -0,0 +1,82 @@
+﻿//see problem at https://code.google.com/codejam/contest/3264486/dashboard#s=p0
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Oversized_Pancake_Flipper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Int32.Parse(Console.ReadLine()); //# input lines
+
+            for (int i = 1; i < t + 1; i++)
+            {
+                //input handling
+                string inputStr = Console.ReadLine();
+                int pivot = inputStr.IndexOf(' ');
+                int flipper = Int32.Parse(inputStr.Substring(pivot + 1));
+                string pancakes = inputStr.Remove(pivot); //trim off flipper width number, leaving just the pancake string
+
+                List<int> flipTrack = new List<int>();
+                List<int> results = tryFlips(pancakes, flipper, 0, 0, flipTrack);
+
+                if (results.Count == 0)
+                {
+                    Console.WriteLine("Case #" + i + ": IMPOSSIBLE");
+                }
+                else
+                {
+                    Console.WriteLine("Case #" + i + ": " + results.Min());
+                }
+            }
+        }
+
+        //attempt all flip permutations and report the lowest flip #
+        //track # flips per solution in 'total' list, then report smallest # (if any)
+        static List<int> tryFlips(string pc, int width, int flips, int depth, List<int> total)
+        {
+            if(pc.IndexOf('-') == -1) //check if solved
+            {
+                total.Add(flips);
+            }
+            else if (width + depth <= pc.Length)
+            {
+                total = tryFlips(pc, width, flips, depth + 1, total);
+
+                string postFlip = pc.Substring(0, depth);
+                for (int i = depth; i < depth + width; i++)
+                {
+                    
+                    if (pc[i].Equals('+'))
+                    {
+                        postFlip += '-';
+                    }
+                    else
+                    {
+                        postFlip += '+';
+                    }
+                }
+                postFlip += pc.Substring(depth + width);
+                total = tryFlips(postFlip, width, flips + 1, depth + 1, total);
+            }
+            return total;
+        }
+    }
+}
+
+/* LOGIC
+ * There are x number of k-width clusters of pancakes, where x = (total pancakes) - k + 1
+ * Example input:   +-+-- 3
+ * Clusters:        +-+
+ *                   -+-
+ *                    +--
+ * x = 5 - 3 + 1 = 3 clusters
+ * In any solution, each cluster will be flipped 0 or 1 times. 2 flips is equivalent to 0 flips state-wise, but is a waste of flips and will give a suboptimal solution.
+ * 
+ * STRATEGY
+ * Attempt every permutation of cluster flips. Report the smallest number of flips that led to a solved state.
+ * If all permutations are attempted and we never reach a state where all pancakes are happy-side-up, the problem is IMPOSSIBLE.
+ */
---
+++ 2017/TidyNumbers.cs
@@ -0,0 +1,98 @@
+﻿//see problem at https://code.google.com/codejam/contest/3264486/dashboard#s=p1
+
+using System;
+
+namespace Tidy_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Int32.Parse(Console.ReadLine()); //# input lines
+
+            for (int i = 1; i < t + 1; i++)
+            {
+                string inputStr = Console.ReadLine();
+                long n = Int64.Parse(inputStr); //must use longs to support 18 digits
+                int powBound = (int)Math.Log10(n); //highest place value in n
+
+                if(powBound != 0) //single digit
+                {                    
+                    n = makeTidy(n, powBound);          
+                }
+
+                Console.WriteLine("Case #" + i + ": " + n);
+            }
+        } 
+
+        static long longPow(long n, long power)
+        {
+            if(power == 0)
+            {
+                return 1;
+            }
+            long result = n;
+            while(power > 1)
+            {
+                result *= n;
+                power--;
+            }
+            return result;
+        }
+
+        //checks if a number is tidy. if not, it will make it so
+        static long makeTidy(long n, int powBound)
+        {
+            long[] digits = new long[powBound + 1]; // holds all digits, whose indices correspond to place value
+            long sum = 0; //used to track value of digits preceding currently-inspected digit
+            int z = 0;
+
+            while(z <= powBound)
+            {
+                if(z == 0)
+                {
+                    digits[z] = n / (longPow(10, powBound));
+                    z++;
+                }
+                else
+                {
+                    sum = 0;
+                    for(int i = 0; i < z; i++)
+                    {
+                        sum += digits[i] * longPow(10, z - i);
+                    }
+                    digits[z] = n / (longPow(10, powBound - z)) - sum;
+
+                    if(digits[z] < digits[z - 1] || digits[z] == 0)
+                    {
+                        n = (sum * longPow(10, powBound - z)) - 1;
+                        z = 0;
+                    }
+                    else
+                    {
+                        z++;
+                    }
+                }
+
+            }
+            return n;
+        }
+    }
+}
+
+/*
+ * Logic
+ * The digit 0 cannot appear in a tidy number. The prompt specifies that there are no leading 0s, and if 0 appears anywhere it must be preceded by a digit of greater value.
+ * Getting rid of the leftmost 0 will be the starting point. Removing any further-right 0s would be pointless, because the 0s to its left would still need to be removed at some point.
+ * Once the leftmost 0 is found, replace all digits following it with 0 and subtract 1. If the given number is 1320405, perform 1320000 - 1 = 1319999.
+ * Now begin at the leftmost digit and move right, checking that each digit is >= its preceding digit.
+ * On encountering a digit that is less than its preceding digit, set it and all digits to its right to 0 and subtract 1. In the above example, 1319999 becomes 1300000 - 1 = 12999999
+ * The resultant number will now have all nondecreasing digits, quite possibly with some trailing 9s.
+ * 
+ * Strategy
+ * The array digits[] stores each digit in n
+ * Use integer division to get n's digits.
+ * If a digit is less than its preceding digit, set this digit and all following it to 0. 
+ * To do this, use each number's index in digits[] to determine its place value, and add them together. Then subtract 1 from this number to clip the trailing 0s.
+ * This will trigger another pass through the while loop surrounding these operations. The while loop will exit if a decreasing digit is never encountered, thus ensuring tidiness.
+ */
